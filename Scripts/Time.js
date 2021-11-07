ae8104886ac10b8abb196fb1f2c010f06b7889bf
unknown <kozhon.amantay@gmail.com>
2021-11-08 00:46:48 +0600

    remake dropdown(not full)

--- Scripts/Time.js
+++ Scripts/Time.js
@@ -1,20 +1,18 @@
-    var timerElement = document.getElementById("timer");
+var timerElement = document.getElementById("timer");
 
+setTimeOnElement();
+
+function setTimeOnElement() {
+  var today = new Date();
+  var time = today.getHours() + ":" + today.getMinutes();
+  timerElement.innerHTML = time;
+  setTimeout(function () {
     setTimeOnElement();
+  }, 10000);
+}
 
-    function setTimeOnElement(){
-        var today = new Date();
-        var time = today.getHours() + ":" + today.getMinutes();
-        timerElement.innerHTML = time;
-        setTimeout(function() {
-        setTimeOnElement();
-        }, 10000);
-    }
-
-    $("div.accordian").accordion({
-        autoHeight: false,
-        collapsible: true,
-        active: false,
-    
-    
-    });
+$("div.accordian").accordion({
+  autoHeight: false,
+  collapsible: true,
+  active: false,
+});
