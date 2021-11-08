a6d58f700bb2a9e88cb270b14778df775a6c11c3
Han <whitehanclean@gmail.com>
2021-11-08 12:19:13 +0600

    add block image accordion

--- Scripts/Time.js
+++ Scripts/Time.js
@@ -1,20 +1,31 @@
-    var timerElement = document.getElementById("timer");
+var timerElement = document.getElementById("timer");
 
-    setTimeOnElement();
+setTimeOnElement();
 
-    function setTimeOnElement(){
-        var today = new Date();
-        var time = today.getHours() + ":" + today.getMinutes();
-        timerElement.innerHTML = time;
-        setTimeout(function() {
+function setTimeOnElement() {
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes();
+    timerElement.innerHTML = time;
+    setTimeout(function () {
         setTimeOnElement();
-        }, 10000);
-    }
+    }, 10000);
+}
 
-    $("div.accordian").accordion({
-        autoHeight: false,
-        collapsible: true,
-        active: false,
-    
-    
-    });
+$("div.accordian").accordion({
+    autoHeight: false,
+    collapsible: true,
+    active: false,
+
+
+});
+$('.accordianIn3').click(
+    function () {
+        $('.scroll').toggleClass('active');
+    }
+);
+
+$('.accordianIn3').click(
+    function() {
+        $('.scroll_image').toggleClass('active');
+    }
+  );
