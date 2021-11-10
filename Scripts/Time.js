3b3d03bd73689443a28e7ad3859553932e3d76ce
unknown <ataydjirgalbaev@gmail.com>
2021-11-10 20:34:59 +0600

    make some blocks disappear when you open another block

--- Scripts/Time.js
+++ Scripts/Time.js
@@ -3,35 +3,41 @@ var timerElement = document.getElementById("timer");
 setTimeOnElement();
 
 function setTimeOnElement() {
-  var today = new Date();
-  var time = today.getHours() + ":" + today.getMinutes();
-  timerElement.innerHTML = time;
-  setTimeout(function () {
-    setTimeOnElement();
-  }, 10000);
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes();
+    timerElement.innerHTML = time;
+    setTimeout(function () {
+        setTimeOnElement();
+    }, 10000);
 }
 
 $("div.accordian").accordion({
-  autoHeight: false,
-  collapsible: true,
-  active: false,
+    autoHeight: false,
+    collapsible: true,
+    active: false,
 });
 $(".accordianIn3Fir").click(function () {
-  $(".scroll").toggleClass("active");
+    $(".scroll").toggleClass("active");
+    $(".window__sort-btn").toggleClass("dis-none");
+    $(".window__registration-btn").toggleClass("dis-none");
+    $(".window__search").toggleClass("dis-none");
+    $(".window__image_left").toggleClass("dis-block");
+    $(".window__image_right").toggleClass("dis-block");
+    $(".window__image_right").toggleClass("bg-grey");
 });
 
 $(".accordianIn3Fir").click(function () {
-  $(".scroll_image").toggleClass("active");
+    $(".scroll_image").toggleClass("active");
 });
 
 $(".accordianIn3Fir").click(function () {
-  $(".scroll_flex").toggleClass("active");
+    $(".scroll_flex").toggleClass("active");
 });
 
 $(".scroll_img").click(function () {
-  $(".window-absolute ").toggleClass("active");
+    $(".window-absolute ").toggleClass("active");
 });
 
 $(".window__images").click(function () {
-  $(".window-absolute ").toggleClass("active");
+    $(".window-absolute ").toggleClass("active");
 });
