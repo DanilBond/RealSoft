710618217da2e02c1b3e7e224a78f080165aa6cb
unknown <kozhon.amantay@gmail.com>
2021-11-18 22:31:56 +0600

    add dark theme first to themeswitcher

--- Scripts/ThemeSwitcher.js
+++ Scripts/ThemeSwitcher.js
@@ -1,15 +1,18 @@
 let links = [
-    "style.css",
-     "Themes/dark-theme-second.css",
-      "Themes/neutral-theme1.css",
-      "Themes/neutral-theme2.css",
-      "Themes/neutral-theme3.css"];
+  "style.css",
+  "Themes/dark-theme-first.css",
+  "Themes/dark-theme-second.css",
+  "Themes/neutral-theme1.css",
+  "Themes/neutral-theme2.css",
+  "Themes/neutral-theme3.css",
+];
 
 let currentTheme = 0;
 
-function switchThemes(){
-    currentTheme++;
-    if(currentTheme >= links.length)
-        currentTheme=0;
-    let link = document.querySelector("#globalStyles").setAttribute("href", links[currentTheme]);
+function switchThemes() {
+  currentTheme++;
+  if (currentTheme >= links.length) currentTheme = 0;
+  let link = document
+    .querySelector("#globalStyles")
+    .setAttribute("href", links[currentTheme]);
 }
